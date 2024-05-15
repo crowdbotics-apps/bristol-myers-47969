bd97c1944370926907d7886b82e41de741036fe3
Crowdbotics <team@crowdbotics.com>
2024-05-15 20:34:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bristol-myers-47969",
+  url: "https://bristol-myers-47969.botics.co",
+  api: "https://bristol-myers-47969.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
